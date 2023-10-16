73421493b63d2db519efc164546efea6f8eef330
zloyaloha <cutevf17@gmail.com>
2023-10-16 18:25:28 +0300

    del build

--- 1/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zloyaloha/Yandex.Disk/MAI/OOP/1
-# Build directory: /home/zloyaloha/Yandex.Disk/MAI/OOP/1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(BASIC "/home/zloyaloha/Yandex.Disk/MAI/OOP/1/build/tests1")
-set_tests_properties(BASIC PROPERTIES  _BACKTRACE_TRIPLES "/home/zloyaloha/Yandex.Disk/MAI/OOP/1/CMakeLists.txt;17;add_test;/home/zloyaloha/Yandex.Disk/MAI/OOP/1/CMakeLists.txt;0;")
