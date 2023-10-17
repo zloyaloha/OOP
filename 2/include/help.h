3551dc194cdb798b42dd5d4b0d3a1ef55042fe06
zloyaloha <cutevf17@gmail.com>
2023-10-18 00:07:26 +0300

    del l1, l2

--- 2/include/help.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-#include <iostream>
-
-size_t CharToInt(const char &c);
-char IntToChar(const int &c);
-std::pair<char, char> Adding(const char &a, const char &b, const char &c);
-std::pair<char, char> Substruction(const char &fwhat, const char &what, const char &mod);
